2bcb92e33fa2d8affaf82eb7004da56b85f52abb
Martin Charlesworth <martin@martincharlesworth.net>
2019-04-26 11:05:18 +0100

    Initial commit

---
+++ src/main/java/com/example/ResourceServer.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+@EnableOAuth2Client
+@Order(200)
+public class ResourceServer extends WebSecurityConfigurerAdapter {
+
+    @RequestMapping({"/user", "/me"})
+    public Map<String, String> user(Principal principal, HttpServletRequest request) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", principal.getName());
+        map.put("JSessionID = ", request.getSession().getId());
+        return map;
+    }
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/me").authorizeRequests().anyRequest().authenticated();
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServer.class, args);
+    }
+
+}
